26e7798175bd989286661c37d2fae77d7489dd2a
hcs <hannahemcs@gmail.com>
2023-11-29 18:56:04 +0100

    Create Order class

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,48 @@
+package org.example;
+
+public class Order {
+    private int id;
+    private int productId;
+    private int quantity;
+    private String customerDetails;
+    public Order(){}
+
+    public Order(int id, int productId, int quantity, String customerDetails) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.customerDetails = customerDetails;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCustomerDetails() {
+        return customerDetails;
+    }
+
+    public void setCustomerDetails(String customerDetails) {
+        this.customerDetails = customerDetails;
+    }
+}
